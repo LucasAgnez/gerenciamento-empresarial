31cce402ccc1b9716dd57719c5619cc4f5ed20b2
daniel.colao.700 <daniel.colao.700@ufrn.edu.br>
2023-09-07 17:51:46 -0300

    initial commit

--- src/pages/Equipe/Equipe.jsx
+++ src/pages/Equipe/Equipe.jsx
@@ -2,8 +2,12 @@ import FuncionarioMiniatura from "../../components/FuncionarioMiniatura/Funciona
 import styles from "./Equipe.module.css";
 import { FiSearch } from "react-icons/fi";
 import { FaFilter } from "react-icons/fa";
+import { BsFillPencilFill, BsFillTrashFill, BsEyeFill } from "react-icons/bs"
+import data from "./mock-data.json"
+
 
 const Equipe = () => {
+
   return (
     <main className={styles.equipe}>
       <section className={styles.cabecalho}>
@@ -17,9 +21,49 @@ const Equipe = () => {
           <FaFilter style={{margin: 0}}/>
         </button>
       </section>
+
+      <caption>Funcionários</caption>
+      <table border={1}>
+
+        <thead>
+          <tr className={styles.tr}>
+            <th className={styles.tableHeader}>ID</th>
+            <th className={styles.tableHeader}>Nome</th>
+            <th className={styles.tableHeader}>Departamento</th>
+            <th className={styles.tableHeader}>CPF</th>
+            <th className={styles.tableHeader}>Email</th>
+            <th className={styles.tableHeader}>Salário</th>
+            <th className={styles.tableHeader}>Ações</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {data.map( (item, index) => (
+            //Uso de parenteses por estarmos lidando com uma expressao (html)
+            //Chaves seria para código
+            <tr key={index} align='center'>
+                <td className={styles.td}>{index}</td>
+                <td className={styles.td}>{item.nome}</td>
+                <td className={styles.td}>{item.departamento}</td>
+                <td className={styles.td}>{item.cpf}</td>
+                <td className={styles.td}>{item.email}</td>
+                <td className={styles.td}>{item.salario}</td>
+                <td className={styles.td}>
+                  <span>
+                    <BsEyeFill />
+                    <BsFillPencilFill />
+                    <BsFillTrashFill />
+                  </span>
+                </td>
+            </tr>
+          ))}
+
+        </tbody>
+
+      </table>
       <section className={styles.listaEquipe}>
         <FuncionarioMiniatura
-          nome="Jociane Barbosa"
+          nome="Daniel Sehn Colao"
           id="10500312"
           cargo="gestor de vendas"
           idade="30"
