a0bcdce85c95540679127c73c98e657e666a704e
Lucas Agnez <lucasagnez@hotmail.com>
2023-10-26 15:58:41 -0300

    adiciona tela de venda

--- src/components/ProdutoMiniatura/ProdutoMiniatura.jsx
+++ src/components/ProdutoMiniatura/ProdutoMiniatura.jsx
@@ -1,27 +1,68 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./ProdutoMiniatura.module.css";
+import { Button } from "@mui/material";
+import { FaMinusCircle, FaPlusCircle} from "react-icons/fa";
+import { useState } from "react";
 
 const ProdutoMiniatura = (props) => {
   const navigate = useNavigate();
-  const { id, nome, qntd } = props;
+  const { id, nome, qntd, vendas } = props;
+  const [ contador, setContador ] = useState(0)
+  const [ qtd, setQtd ] = useState(qntd)
+  const [ inc, setInc ] = useState(qntd != 0 )
+  const [ dec, setDec ] = useState(false)
 
   //onClick={navigate(`/gerenciamento/estoque/${id}`)
-
+  function incrementa(){
+    if(qtd == 0){
+      return
+    }
+    setInc(true)
+    setDec(true)
+    setContador(contador + 1)
+    setQtd(qtd-1)
+    if(qtd-1==0){
+      setInc(false)
+    }
+  }
+  function decrementa(){
+    if(contador==0){
+      return
+    }
+    setContador(contador-1)
+    setQtd(qtd+1)
+    setInc(true)
+    if(contador-1==0){
+      setDec(false)
+    }
+  }
   return (
     <div
       className={styles.produtoMiniatura}
-      onClick={() => navigate(`/gerenciamento/estoque/${id}`)}
     >
-      <section className={styles.conteudo}>
+      <section className={styles.conteudo}
+      onClick={() => navigate(`/gerenciamento/estoque/${id}`)}>
         <img
           className={styles.image}
           src="404image"
         />
         <section className={styles.infos}>
           <p>{nome}</p>
-          <p>{qntd}</p>
+          <p>{qtd}</p>
         </section>
       </section>
+      { vendas ? 
+      <>
+        <section className={styles.butoes}>
+          <Button disabled={!dec} onClick={() => decrementa()}size='small' variant="text" ><FaMinusCircle/></Button>
+          <Button disabled={!inc} onClick={() => incrementa()}size='small' variant="text"><FaPlusCircle/></Button>
+        </section>
+        <section>
+          <b>{contador}</b>
+        </section>
+      </>
+      : null
+      }
     </div>
   );
 };
--- src/pages/Estoque/Estoque.jsx
+++ src/pages/Estoque/Estoque.jsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import Filtro from "../../components/Filtro/Filtro";
 import ProdutoMiniatura from "../../components/ProdutoMiniatura/ProdutoMiniatura";
 import styles from "./Estoque.module.css";
-import { TextField } from '@mui/material/';
+import { TextField, Button } from '@mui/material/';
 import { FiSearch } from "react-icons/fi";
 
 const Estoque = () => {
   const tags=["esporte", "casual", "masculino", "feminino"]
+  const [vendas, setVendas] = useState(false)
   return (
     <main className={styles.estoque}>
       <section className={styles.cabecalho}>
+        <Button 
+          color="success"
+          variant="contained"
+          onClick={() => setVendas(!vendas)}>
+            Registrar Venda
+        </Button>
         <section className={styles.pesquisa}>
           <div className={styles.barraPesquisa}>
           <TextField id="pesquisa" variant="standard"/>
@@ -18,12 +26,17 @@ const Estoque = () => {
         <Filtro slider={true} labels={tags}/>
       </section>
       <section className={styles.estoqueGrid}>
-        <ProdutoMiniatura nome="aaa" qntd="0" id="db29126v" />
-        <ProdutoMiniatura nome="aaa" qntd="0" id="db29126v" />
-        <ProdutoMiniatura nome="aaa" qntd="0" id="db29126v" />
-        <ProdutoMiniatura nome="aaa" qntd="0" id="db29126v" />
-        <ProdutoMiniatura nome="aaa" qntd="0" id="db29126v" />
+        <ProdutoMiniatura vendas={vendas} nome="aaa" qntd="10" id="db29126v" />
+        <ProdutoMiniatura vendas={vendas} nome="aaa" qntd="5" id="db29126v" />
+        <ProdutoMiniatura vendas={vendas} nome="aaa" qntd="6" id="db29126v" />
+        <ProdutoMiniatura vendas={vendas} nome="aaa" qntd="2" id="db29126v" />
+        <ProdutoMiniatura vendas={vendas} nome="aaa" qntd="0" id="db29126v" />
       </section>
+      { vendas ? <Button 
+          color="success"
+          variant="contained">
+            Concluir
+        </Button> : null}
     </main>
   );
 };
